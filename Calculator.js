64537b46a93c889f5dbc3e98d73091cefdc2fa87
Siddharth <siddharthisrani123@gmail.com>
2025-06-10 19:33:47 +0530

    first commit

---
+++ Calculator.js
@@ -0,0 +1,39 @@
+ let store=""
+
+ let cal=(value)=>{
+
+let Display=document.querySelector("#screen")
+
+store=store+value  //  ""+1=> 1     1+ +=> 1+   1+ + 2=> 1+2
+
+Display.innerHTML=store
+
+ }
+
+ let res=()=>{
+    
+let Display=document.querySelector("#screen")
+
+     store=   eval(store).toString() 
+
+     Display.innerHTML=store
+
+ }
+
+
+ let ac=()=>{
+let Display=document.querySelector("#screen")
+    store=""
+Display.innerHTML=store
+
+ }
+
+
+ let del=()=>{
+let Display=document.querySelector("#screen")
+
+ store= store.slice(0,-1)
+Display.innerHTML=store
+   
+
+ }
